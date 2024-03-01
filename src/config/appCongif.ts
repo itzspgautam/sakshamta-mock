5dc7f57066a277422af6b555d519745da51bc1da
itzspgautam <spgautam.vfx@gmail.com>
2024-03-02 02:31:21 +0530

    Fixes some bugs

--- src/config/appCongif.ts
+++ src/config/appCongif.ts
@@ -1,5 +1,5 @@
 const AppConfig = {
-  API: "http://localhost:3000",
+  API: "https://sakshamta.onrender.com",
   CLOUDINARY_API: "https://api.cloudinary.com/v1_1/dcklidvvw/image/upload",
   CL_IMAGE_PRESET: "sakshamta-photo",
   CL_SIGN_PRESET: "sakshamta-sign",
