c909579423b05030c5102a6112c4967ebd2ea982
itzspgautam <spgautam.vfx@gmail.com>
2024-03-02 16:10:19 +0530

    Live for main domain

--- src/config/appCongif.ts
+++ src/config/appCongif.ts
@@ -1,5 +1,5 @@
 const AppConfig = {
-  API:"https://sakshamta.onrender.com", //'http://localhost:3000',//
+  API:"https://bsebsakshamta.co.in/", //'http://localhost:3000',//
   CLOUDINARY_API: "https://api.cloudinary.com/v1_1/dcklidvvw/image/upload",
   CL_IMAGE_PRESET: "sakshamta-photo",
   CL_SIGN_PRESET: "sakshamta-sign",
